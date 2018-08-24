a6d38e308a49b8531df6a461b98e2b1aa1aacf37
Sylvain Boily <sylvainboilydroid@gmail.com>
2018-08-24 10:39:42 -0400

    add checkAuth

---
+++ src/api/auth/check-auth.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import wazo from '../../config';
+import version from './version';
+
+const handleResponse = (callback) => {
+  wazo.token = null;
+
+  if (callback) {
+    callback(wazo.token);
+  }
+};
+
+export default (params) => {
+  if (wazo.token) {
+    const url = `https://${wazo.server}/api/auth/${version}/token/${wazo.token}`;
+
+    axios.head(url)
+      .then(handleResponse(params.callback));
+  }
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import client from './application';
 import init from './config/init';
 import logIn from './api/auth/log-in';
 import logOut from './api/auth/log-out';
+import checkAuth from './api/auth/check-auth';
 
 import calls from './api/applications/calls';
 import hangupCall from './api/applications/hangup-call';
@@ -18,6 +19,7 @@ export default {
   init,
   logIn,
   logOut,
+  checkAuth,
   calls,
   hangupCall,
   answerCall,
