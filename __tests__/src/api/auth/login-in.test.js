e66ddcac0c4d195335f497f8112f825a8796a18b
Emmanuel Quentin <manu.quentin@gmail.com>
2018-09-12 16:35:01 +0200

    Add simple test on sign-in request

---
+++ __tests__/src/api/auth/login-in.test.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import logIn from '../../../../src/api/auth/log-in';
+
+const mockedResponse = { data: { data: { token: 1 } } };
+
+jest.mock('axios', () => ({
+  post: jest.fn(() => Promise.resolve(mockedResponse)),
+}));
+
+it("should call wazo's token API with default values", () => {
+  logIn();
+  const defaultServer = null;
+  const defaultVersion = '0.1';
+  const data = { backend: 'wazo_user', expiration: 3600 };
+  const config = { auth: { username: undefined, password: undefined } };
+
+  expect(axios.post).toBeCalledWith(
+    `https://${defaultServer}/api/auth/${defaultVersion}/token`,
+    data,
+    config,
+  );
+});
