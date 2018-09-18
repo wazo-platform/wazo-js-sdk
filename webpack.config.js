653b7ab9abaf8747eb57ad6b422d96bdb26e95b1
Emmanuel Quentin <manu.quentin@gmail.com>
2018-09-18 18:00:20 +0200

    Fix webpack build to run in node env

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     filename: 'wazo-sdk.js',
     library: '@wazo/sdk',
     libraryTarget: 'umd',
+    libraryExport: 'default',
+    // Workaround for webpack 4 umd bug (@see: https://github.com/webpack/webpack/issues/6522)
+    globalObject: "typeof self !== 'undefined' ? self : this",
   },
   devtool: 'source-map',
   externals: ['axios', 'sip.js', 'reconnecting-websocket'],
