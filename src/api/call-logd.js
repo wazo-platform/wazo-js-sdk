0debbc987075b3d39dd4cf686c248f04f689e40c
Emmanuel Quentin <manu.quentin@gmail.com>
2018-10-03 17:13:11 +0200

    Accept Date instead of string in parameter

--- src/api/call-logd.js
+++ src/api/call-logd.js
@@ -1,6 +1,6 @@
 /* @flow */
 import ApiRequester from '../utils/api-requester';
-import type { Token, DateString } from '../domain/types';
+import type { Token } from '../domain/types';
 import CallLog from '../domain/CallLog';
 
 export default (client: ApiRequester, baseUrl: string) => ({
@@ -16,7 +16,9 @@ export default (client: ApiRequester, baseUrl: string) => ({
       .then(response => CallLog.parseMany(response));
   },
 
-  listCallLogsFromDate(token: Token, from: DateString, number: number): Promise<Array<CallLog>> {
-    return client.get(`${baseUrl}/users/me/cdr`, { from, number }, token).then(response => CallLog.parseMany(response));
+  listCallLogsFromDate(token: Token, from: Date, number: number): Promise<Array<CallLog>> {
+    return client
+      .get(`${baseUrl}/users/me/cdr`, { fromfrom: from.toISOString(), number }, token)
+      .then(response => CallLog.parseMany(response));
   }
 });
