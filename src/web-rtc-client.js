e4542f2e625e0460b34f8f47a9fb888ed6077434
Emmanuel Quentin <manu.quentin@gmail.com>
2019-01-31 08:35:20 -0500

    Remove stun servers

--- src/web-rtc-client.js
+++ src/web-rtc-client.js
@@ -74,9 +74,6 @@ export default class WebRTCClient {
         {
           urls: [
             'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-            'stun:stun3.l.google.com:19302',
             'stun:stun4.l.google.com:19302'
           ]
         }
