cbf8933d0e1d6295fe6743cbf18ede6e4ca40272
Emmanuel Quentin <manu.quentin@gmail.com>
2019-02-11 11:26:46 -0500

    Remove fat arrow in class methods that make the builder fails

--- src/utils/CallbacksHandler.js
+++ src/utils/CallbacksHandler.js
@@ -7,31 +7,31 @@ export default class CallbacksHandler {
     this.callbacks = {};
   }
 
-  on = (event: string, callback: Function) => {
+  on(event: string, callback: Function){
     if (!(event in this.callbacks)) {
       this.callbacks[event] = [];
     }
 
     this.callbacks[event].push(callback);
-  };
+  }
 
-  clear = () => {
+  clear() {
     this.callbacks = {};
   };
 
   // trigger callback registered with .on('name', ...)
-  triggerCallback = (eventName: string, ...args: Array<any>) => {
+  triggerCallback (eventName: string, ...args: Array<any>) {
     // Add event name at last argument, so we can know the event name if we do on('*', ...)
     args.push(eventName);
 
     if (this.callbacks['*']) {
-      this.callbacks['*'].forEach(cb => cb.apply(undefined, args));
+      this.callbacks['*'].forEach(cb => cb(...args));
     }
 
     if (!(eventName in this.callbacks)) {
       return null;
     }
 
-    return this.callbacks[eventName].forEach(cb => cb.apply(undefined, args));
+    return this.callbacks[eventName].forEach(cb => cb(...args));
   };
 }
