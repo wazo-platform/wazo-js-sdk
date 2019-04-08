e3d4fa719f97aa295bf12a318c315e02730283f1
Sébastien Dufour-Beauséjour <sebastien.dufourbeausejour@gmail.com>
2019-04-08 14:34:22 -0400

    Parse all line information from response

--- src/domain/Line.js
+++ src/domain/Line.js
@@ -8,9 +8,15 @@ type Extension = {
   links?: Array<{ href: string, rel: string }>,
 };
 
+type Endpoint = {
+  id: number,
+  links: Array<{href: string, rel: string}>
+}
+
 type LineResponse = {
-  endpoint_custom: ?string,
-  endpoint_sccp: ?string,
+  endpoint_custom: ?Endpoint,
+  endpoint_sccp: ?Endpoint,
+  endpoint_sip: ?Endpoint,
   extensions: Array<Extension>,
   id: number,
 };
@@ -18,16 +24,25 @@ type LineResponse = {
 type LineArguments = {
   id: number,
   extensions: Array<Extension>,
+  endpointCustom: ?Endpoint,
+  endpointSccp: ?Endpoint,
+  endpointSip: ?Endpoint,
 };
 
 export default class Line {
   id: number;
   extensions: Array<Extension>;
+  endpointCustom: Endpoint | null;
+  endpointSccp: Endpoint | null;
+  endpointSip: Endpoint | null;
 
   static parse(plain: LineResponse): Line {
     return new Line({
       id: plain.id,
       extensions: plain.extensions,
+      endpointCustom: plain.endpoint_custom || null,
+      endpointSccp: plain.endpoint_sccp || null,
+      endpointSip: plain.endpoint_sip || null,
     });
   }
 
@@ -39,8 +54,11 @@ export default class Line {
     return this.id === line.id;
   }
 
-  constructor({ id, extensions }: LineArguments = {}) {
+  constructor({ id, extensions, endpointCustom, endpointSccp, endpointSip }: LineArguments = {}) {
     this.id = id;
     this.extensions = extensions;
+    this.endpointCustom = endpointCustom || null;
+    this.endpointSccp = endpointSccp || null;
+    this.endpointSip = endpointSip || null;
   }
 }
