a7178935c6b2d4398999648935cba195cc7a0ad7
Emmanuel Quentin <manu.quentin@gmail.com>
2020-05-28 17:32:33 -0400

    Fix line length

--- src/api/application.js
+++ src/api/application.js
@@ -90,7 +90,8 @@ export default (client: ApiRequester, baseUrl: string) => ({
     client.put(`${baseUrl}/${applicationUuid}/calls/${callId}/mute/stop`, {}, null, ApiRequester.successResponseParser),
 
   sendDTMFCall: (applicationUuid: string, callId: number, digits: number) =>
-    client.put(`${baseUrl}/${applicationUuid}/calls/${callId}/dtmf`, { digits }, null, ApiRequester.successResponseParser),
+    client.put(`${baseUrl}/${applicationUuid}/calls/${callId}/dtmf`, { digits }, null,
+      ApiRequester.successResponseParser),
 
   addCallNodes: (applicationUuid: string, nodeUuid: string, callId: string): Promise<Boolean> =>
     client.put(
