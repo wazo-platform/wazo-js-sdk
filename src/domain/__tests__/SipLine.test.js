1c0e26e688f2b4dff1a55c17f3cde1a95a0a750c
Francis Chartrand <chartrandf@bonyweb.com>
2020-09-10 09:25:31 -0400

    Give better name to the tests 🙈

--- src/domain/__tests__/SipLine.test.js
+++ src/domain/__tests__/SipLine.test.js
@@ -2,33 +2,37 @@ import SipLine, { MINIMUM_WAZO_ENGINE_VERSION_FOR_PJSIP } from '../SipLine';
 import Session from '../Session';
 
 describe('SipLine domain', () => {
-  it('CHAN_SIP : can tell if we have videoConference capabilities', () => {
-    const sess = new Session({
-      token: 'ref-12345',
-      uuid: '1234',
-      engineVersion: '20.12',
-    });
-    const getSipLine = options => new SipLine({ options });
+  describe('Endpoint (20.13 and higher)', () => {
+    it('can tell if we have videoConference capabilities', () => {
+      const sess = new Session({
+        token: 'ref-12345',
+        uuid: '1234',
+        engineVersion: MINIMUM_WAZO_ENGINE_VERSION_FOR_PJSIP,
+      });
+      const getSipLine = endpointSectionOptions => new SipLine({ endpointSectionOptions });
 
-    expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
-    expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '1']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_audio_streams', '2']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_video_streams', '2']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_audio_streams', '2'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+      expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+      expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '1']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_audio_streams', '2']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_video_streams', '2']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_audio_streams', '2'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+    });
   });
 
-  it('PJSIP : can tell if we have videoConference capabilities', () => {
-    const sess = new Session({
-      token: 'ref-12345',
-      uuid: '1234',
-      engineVersion: MINIMUM_WAZO_ENGINE_VERSION_FOR_PJSIP,
-    });
-    const getSipLine = endpointSectionOptions => new SipLine({ endpointSectionOptions });
+  describe('Old Endpoint', () => {
+    it('can tell if we have videoConference capabilities', () => {
+      const sess = new Session({
+        token: 'ref-12345',
+        uuid: '1234',
+        engineVersion: '20.12',
+      });
+      const getSipLine = options => new SipLine({ options });
 
-    expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
-    expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '1']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_audio_streams', '2']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_video_streams', '2']]).hasVideoConference(sess)).toBe(false);
-    expect(getSipLine([['max_audio_streams', '2'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+      expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+      expect(getSipLine([['max_audio_streams', '1'], ['max_video_streams', '1']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_audio_streams', '2']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_video_streams', '2']]).hasVideoConference(sess)).toBe(false);
+      expect(getSipLine([['max_audio_streams', '2'], ['max_video_streams', '2']]).hasVideoConference(sess)).toBe(true);
+    });
   });
 });
