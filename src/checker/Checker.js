96e747786bfaa2078676b68689a39869e1be79b3
Emmanuel Quentin <manu.quentin@gmail.com>
2020-10-26 10:55:03 -0400

    Log engine checks

--- src/checker/Checker.js
+++ src/checker/Checker.js
@@ -2,8 +2,11 @@
 
 import type Session from '../domain/Session';
 import WazoApiClient from '../api-client';
+import IssueReporter from '../service/IssueReporter';
 
 import checks from './checks/index';
+
+const logger = IssueReporter.loggerFor('engine-check');
 
 class Checker {
   session: Session;
@@ -19,18 +22,28 @@ class Checker {
   async check(onCheckBegin: Function = () => {}, onCheckResult: Function = () => {}) {
     await this._addEngineVersion();
 
+    logger(logger.INFO, 'Engine check starting');
+
     for (let i = 0; i < this.checks.length; i++) {
       const { name, check } = this.checks[i];
+
+      logger(logger.INFO, `Checking ${name} ...`);
 
       onCheckBegin(name);
       try {
         // eslint-disable-next-line no-await-in-loop
         const result = await check(this.server, this.session);
+        logger(logger.INFO, `Checking ${name} success.${result ? ` : ${result}` : ''}`);
+
         onCheckResult(name, result);
       } catch (e) {
+        logger(logger.INFO, `Checking ${name} failure : ${e.message}`);
+
         onCheckResult(name, e);
       }
     }
+
+    logger(logger.INFO, 'Engine check done.');
   }
 
   addCheck(check: Object) {
