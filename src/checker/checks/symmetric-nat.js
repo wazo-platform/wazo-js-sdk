45a5abcedf2863f8b32132bedf25c1a9a0f357c0
Emmanuel Quentin <manu.quentin@gmail.com>
2020-11-03 17:48:02 -0500

    Improve symmetric nat check

--- src/checker/checks/symmetric-nat.js
+++ src/checker/checks/symmetric-nat.js
@@ -1,6 +1,8 @@
 /* global MediaStream, RTCPeerConnection */
 import { parseCandidate } from '../../utils/webrtc';
 
+const timeoutDuration = 5000;
+
 // @see https://webrtchacks.com/symmetric-nat/
 export default {
   name: 'Symmetric NAT',
@@ -9,8 +11,17 @@ export default {
       return resolve('Skipped on node');
     }
 
+    const candidates = {};
+    const rawCandidates = [];
+    let nbCandidates = 0;
+
+    const timeout = setTimeout(() => {
+      reject(new Error(`Timed out after ${timeoutDuration}, ${nbCandidates} candidates : ${rawCandidates.join(', ')}`));
+    }, timeoutDuration);
+
     const pc = new RTCPeerConnection({
       iceServers: [
+        { urls: 'stun:stun.wazo.io:443' },
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' },
       ],
@@ -18,9 +29,13 @@ export default {
 
     pc.createDataChannel('wazo-check-nat');
 
-    const candidates = {};
-
     pc.onicecandidate = e => {
+      nbCandidates++;
+
+      if (e.candidate) {
+        rawCandidates.push(e.candidate.candidate);
+      }
+
       if (e.candidate && e.candidate.candidate.indexOf('srflx') !== -1) {
         const cand = parseCandidate(e.candidate.candidate);
         if (!candidates[cand.relatedPort]) candidates[cand.relatedPort] = [];
@@ -28,6 +43,8 @@ export default {
       } else if (!e.candidate) {
         if (Object.keys(candidates).length === 1) {
           const ports = candidates[Object.keys(candidates)[0]];
+
+          clearTimeout(timeout);
 
           if (ports.length === 1) {
             resolve();
