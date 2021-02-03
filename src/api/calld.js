e540167be9dac9e6b4c26ae5e25ebab874c31b8c
Jeff Gauthier <code@bogue.ca>
2021-02-03 11:30:22 -0500

    Lint

--- src/api/calld.js
+++ src/api/calld.js
@@ -157,7 +157,7 @@ export default (client: ApiRequester, baseUrl: string) => ({
 
   sendDTMF: (callId: string, digits: string) =>
     client.put(`${baseUrl}/users/me/calls/${callId}/dtmf?digits=${encodeURIComponent(digits)}`,
-    null, null, ApiRequester.successResponseParser),
+      null, null, ApiRequester.successResponseParser),
 
   // @deprecated: check for engine version >= 20.12 instead
   isAhHocConferenceAPIEnabled: () => client.head(`${baseUrl}/users/me/conferences/adhoc`, null, null,
