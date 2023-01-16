b23302f4347fda7c0b9071a6eb77e011ba84a7e5
Jeff Gauthier <code@bogue.ca>
2023-01-16 11:56:53 -0500

    Set noImplicitAny to true

---
+++ src/types/global.d.ts
@@ -0,0 +1,26 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+
+declare global {
+  var Wazo;
+  var wazoClients;
+  var wazoClientId;
+  var wazoClientToken;
+  var wazoRefreshToken;
+  var wazoRefreshTenantId;
+  var wazoRefreshDomainName;
+  var wazoOnRefreshToken;
+  var wazoOnRefreshTokenError;
+  var wazoRefreshExpiration;
+  var wazoRefreshBackend;
+  var wazoIsMobile;
+  var wazoFetchOptions;
+  var wazoCurrentServer;
+  var wazoAuthInstance;
+  var wazoConfigurationInstance;
+  var wazoDirectoryInstance;
+  var wazoTelephonyInstance;
+  var wazoWebsocketInstance;
+}
+
+export {};
