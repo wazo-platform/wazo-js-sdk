14bc7da5a82cc9562b35a8969970a4c5e27d2b7b
Jeff Gauthier <code@bogue.ca>
2024-01-11 16:17:22 -0500

    Add Agent and other missing exports to root

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,8 @@ export { NewContact, ContactResponse, ContactsResponse, ContactPersonalResponse,
 export { Phone, PhoneEventCallbacks } from './domain/Phone/Phone';
 export { ChatUser, ChatMessageResponse } from './domain/ChatMessage';
 export { Device } from './domain/Device/Device';
-export { WebSocketMessage, PresencesReadEvent } from './types/WebSocketMessage';
 export { default as DebugDevice } from './domain/Device/DebugDevice';
 export { default as Checker } from './checker/Checker';
-export * from './utils/PhoneNumberUtil';
 export { default as ApiRequester } from './utils/api-requester';
 export { DirectorySource as Source, DirectorySources as Sources } from './domain/DirectorySource';
 export { DirectorySource, DirectorySources } from './domain/DirectorySource';
@@ -48,6 +46,9 @@ export { default as AdHocAPIConference } from './domain/AdHocAPIConference';
 export { default as WebSocketClient, SOCKET_EVENTS } from './websocket-client';
 export { default as WebRTCClient } from './web-rtc-client';
 export { default as WazoApiClient } from './api-client';
+export { default as Agent, AgentResponse, AgentArguments } from './domain/Agent';
+export * from './types/WebSocketMessage';
+export * from './utils/PhoneNumberUtil';
 export * from './domain/types';
 
 export default Wazo;
